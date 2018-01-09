d03374e8a7a9be5bf6a8df4a75fa8001454bf70c
hadxu <hadxu123@gmail.com>
2018-01-09 14:27:03 +0800

    add GMM(EM)

--- utils/metric.py
+++ utils/metric.py
@@ -44,9 +44,22 @@ def accuracy_score(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
     return accuracy
 
+
+def calculate_covariance_matrix(X, Y=None):
+    """ Calculate the covariance matrix for the dataset X """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+
+    return np.array(covariance_matrix, dtype=float)
+
+
 def main():
-	x = np.array([1,1,1,1,1,0,0,0,0,0,0,0,0])
-	print(calculate_entropy(x))
+    X = np.array([[0, 0, 0], [1, 0, 1], [1, 0, 0], [1, 1, 0]])
+    print(calculate_covariance_matrix(X))
+    print(np.cov(X.T))
+
 
 if __name__ == '__main__':
 	main()
