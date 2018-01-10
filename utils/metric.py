06f858b2589ad0c37ac249522316549e7fc2a322
hadxu <hadxu123@gmail.com>
2018-01-10 16:58:29 +0800

    add PCA

--- utils/metric.py
+++ utils/metric.py
@@ -53,6 +53,19 @@ def calculate_covariance_matrix(X, Y=None):
     covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
 
     return np.array(covariance_matrix, dtype=float)
+    
+
+def calculate_correlation_matrix(X, Y=None):
+    """ Calculate the correlation matrix for the dataset X """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance = (1 / n_samples) * (X - X.mean(0)).T.dot(Y - Y.mean(0))
+    std_dev_X = np.expand_dims(calculate_std_dev(X), 1)
+    std_dev_y = np.expand_dims(calculate_std_dev(Y), 1)
+    correlation_matrix = np.divide(covariance, std_dev_X.dot(std_dev_y.T))
+
+    return np.array(correlation_matrix, dtype=float)
 
 
 def main():
